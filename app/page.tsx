9c48ea0b57da64109fac360fc2b4a10258684d37
Luciano Oliveira <aulascursos66@gmail.com>
2024-09-21 18:54:03 -0300

    chore: full setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import { Button } from "./_components/ui/button";
+import { Button } from "./_components/ui/button"
 
 export default function Home() {
   return (
     <>
-      <Button>Clique aqui</Button>
+      <h1>Teste</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+        reprehenderit minima sint magnam necessitatibus assumenda accusamus! Hic
+        quidem omnis sit quia explicabo, fugiat consequatur corrupti, deserunt
+        atque, possimus velit odio.
+      </p>
+      <Button>Shadcn button</Button>
     </>
-  );
+  )
 }
