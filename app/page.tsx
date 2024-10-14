3d3d474e7a0ce147dbd0bac3fe5460d91c5682fd
Luciano Oliveira <aulascursos66@gmail.com>
2024-10-14 01:24:11 -0300

    feat: add Header component, searchbar and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,34 @@
+import Image from "next/image"
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
 
 export default function Home() {
   return (
-    <>
-      <h1>Teste</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
-        reprehenderit minima sint magnam necessitatibus assumenda accusamus! Hic
-        quidem omnis sit quia explicabo, fugiat consequatur corrupti, deserunt
-        atque, possimus velit odio.
-      </p>
-      <Button>Shadcn button</Button>
-    </>
+    <div>
+      <Header />
+
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Luciano!</h2>
+        <p>Segunda-feira, 05 de agosto</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca"></Input>
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            fill
+            className="rounded-xl object-cover"
+            src="/banner-01.svg"
+            alt="Agende nos melhores com B-barber"
+          />
+        </div>
+      </div>
+    </div>
   )
 }
