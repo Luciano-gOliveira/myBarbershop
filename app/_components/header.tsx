78ba51d8280fc4ca7c6e6e8cd964da97255f73b9
Luciano Oliveira <aulascursos66@gmail.com>
2024-10-14 22:33:50 -0300

    feat: add BarbershopItem component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
         <CardContent className="flex flex-row items-center justify-between p-5">
           <Image alt="Virgo Barber" src="/logo.svg" height={18} width={120} />
           <Button size="icon" variant="outline">
-            <MenuIcon></MenuIcon>
+            <MenuIcon />
           </Button>
         </CardContent>
       </Card>
