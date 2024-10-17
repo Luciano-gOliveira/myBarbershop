275d06466060f3a2cfe0f3419809337ac34b4ef8
Luciano Oliveira <aulascursos66@gmail.com>
2024-10-17 02:38:02 -0300

    feat: quicksearchbar in constants folder  and BookingItem component

---
+++ app/_constants/search.ts
@@ -0,0 +1,33 @@
+interface QuickSearchOptions {
+  imageUrl: string
+  title: string
+}
+
+const quickSearchOptions: QuickSearchOptions[] = [
+  {
+    imageUrl: "/cabelo.svg",
+    title: "Cabelo",
+  },
+  {
+    imageUrl: "/barba.svg",
+    title: "Barba",
+  },
+  {
+    imageUrl: "/acabamento.svg",
+    title: "Acabamento",
+  },
+  {
+    imageUrl: "/sobrancelha.svg",
+    title: "Sobrancelha",
+  },
+  {
+    imageUrl: "/massagem.svg",
+    title: "Massagem",
+  },
+  {
+    imageUrl: "/hidratacao.svg",
+    title: "Hidratacao",
+  },
+]
+
+export default quickSearchOptions
