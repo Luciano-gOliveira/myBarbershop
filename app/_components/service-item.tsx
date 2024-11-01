e2ebffadd9c6c72f936132cf612d1b73306a9702
Luciano Oliveira <aulascursos66@gmail.com>
2024-11-01 01:36:20 -0300

    feat: ensure bookings are listed on bookings page

--- app/_components/service-item.tsx
+++ app/_components/service-item.tsx
@@ -53,6 +53,7 @@ const TIME_LIST = [
 ]
 
 const getTimeList = (bookings: Booking[]) => {
+  //TODO: não exibir horários no passado
   return TIME_LIST.filter((time) => {
     const hour = Number(time.split(":")[0])
     const minutes = Number(time.split(":")[1])
---
+++ app/bookings/page.tsx
@@ -0,0 +1,79 @@
+import { getServerSession } from "next-auth"
+import Header from "../_components/header"
+import { db } from "../_lib/prisma"
+import { authOptions } from "../_lib/auth"
+import { notFound } from "next/navigation"
+import BookingItem from "../_components/booking-item"
+
+const Bookings = async () => {
+  const session = await getServerSession(authOptions)
+  if (!session?.user) {
+    //mostrar popup de login
+    return notFound()
+  }
+
+  const confirmedBookings = await db.booking.findMany({
+    where: {
+      // eslint-disable-next-line
+      userId: (session.user as any).id,
+      date: {
+        gte: new Date(),
+      },
+    },
+    include: {
+      service: {
+        include: {
+          barbershop: true,
+        },
+      },
+    },
+    orderBy: {
+      date: "asc",
+    },
+  })
+
+  const concludeBookings = await db.booking.findMany({
+    where: {
+      // eslint-disable-next-line
+      userId: (session.user as any).id,
+      date: {
+        lt: new Date(),
+      },
+    },
+    include: {
+      service: {
+        include: {
+          barbershop: true,
+        },
+      },
+    },
+    orderBy: {
+      date: "asc",
+    },
+  })
+
+  return (
+    <>
+      <Header />
+      <div className="space-y-3 p-5">
+        <div className="border-b border-solid pb-5">
+          <h1 className="text-xl font-bold">Agendamentos</h1>
+        </div>
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Confirmados
+        </h2>
+        {confirmedBookings.map((booking) => (
+          <BookingItem key={booking.id} booking={booking} />
+        ))}
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Finalizados
+        </h2>
+        {concludeBookings.map((booking) => (
+          <BookingItem key={booking.id} booking={booking} />
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default Bookings
